fa45a9eade3bb465adf7a761652397885b92dc83
carlogilmar <carlogilmar12@gmail.com>
2019-02-02 20:35:52 -0600

    Adding Nemias exercise

---
+++ exercise1.py
@@ -0,0 +1,18 @@
+class Worker:
+     def __init__(self, amount):
+         self.salary = amount
+
+     def bonus_salary( self ):
+       if( self.salary < 4000 ):
+         return self.salary + (self.salary * 0.15)
+       else:
+         return self.salary + (self.salary * 0.08)
+
+     def get_salary_with_bonus( self ):
+       return self.salary + self.bonus_salary()
+
+     def show_data(self):
+         print(":: This worker have the follow amounts ::")
+         print("Salary: ", self.salary)
+         print("Bonus: ", self.bonus_salary())
+
---
+++ imple.py
@@ -0,0 +1,7 @@
+from exercise1 import Worker
+
+print("Welcome!!")
+nemias = Worker(9000)
+nemias.show_data()
+carlo = Worker(3500)
+carlo.show_data()
